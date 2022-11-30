39c42d378ba37dd648c5c58da9ad0d369c1a3b6b
kevin <dkevinroth@gmail.com>
2022-11-30 15:41:58 -0300

    63

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
@@ -6,9 +7,36 @@ import { Component } from '@angular/core';
 })
 export class MainPageComponent {
   
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
   agregar(){
-    console.log('asd');
-    
+    if(this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+    console.log(this.nuevo);
+  }
+
+  cambiarNombre(){
+
   }
 
 }
