bf9c9c484f65d72040e06335532a013d3b361aa2
kevdroth <91353251+kevdroth@users.noreply.github.com>
2022-11-30 09:27:09 -0300

    59

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModules } from './heroes/heroes.module';
 import { ContadorModules } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ContadorModules } from './contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModules,
-    ContadorModules
+    ContadorModules,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
