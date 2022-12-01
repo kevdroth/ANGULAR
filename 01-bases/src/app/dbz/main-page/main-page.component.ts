4b6364345a97a1a268f60259fc7e72d4d1e91762
kevdroth <91353251+kevdroth@users.noreply.github.com>
2022-12-01 09:41:34 -0300

    71

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -7,29 +7,13 @@ import { DbzService } from '../services/dbz.service';
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
-  
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
 
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
   }
 
-  agregarNuevoPersonaje( argumento: Personaje ){
-    this.personajes.push(argumento);
-  }
-
-  constructor( private dbzService: DbzService ){
-
+  constructor(){
   }
 
 }
--- 01-bases/src/app/dbz/services/dbz.service.ts
+++ 01-bases/src/app/dbz/services/dbz.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from "@angular/core";
+import { Personaje } from "../interfaces/dbz.interface";
 
 
 @Injectable()
 export class DbzService {
-    constructor(){
-        console.log('servicio');
-        
+
+    private _personajes: Personaje[] = [
+        {
+          nombre: 'Goku',
+          poder: 15000
+        },
+        {
+          nombre: 'Vegeta',
+          poder: 7500
+        }
+      ];
+
+    get personajes(): Personaje[]{
+        return [...this._personajes];
     }
+      
+    constructor(){
+        console.log('servicio ok');
+    }
+
+    agregarPersonaje( personaje: Personaje ){
+        this._personajes.push(personaje);
+    }
+
 }
