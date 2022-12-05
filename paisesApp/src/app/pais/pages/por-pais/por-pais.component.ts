dc445dd6770c864583e0fa25ebb3000ea1283718
kevdroth <91353251+kevdroth@users.noreply.github.com>
2022-12-05 14:31:13 -0300

    111

---
+++ paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+
+  constructor(private paisService: PaisService){}
+
+  buscar(){
+    this.hayError = false;
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe( (paises) => {
+        console.log(paises);
+      },(err) => {
+        this.hayError = true;
+      });
+  }
+
+}
